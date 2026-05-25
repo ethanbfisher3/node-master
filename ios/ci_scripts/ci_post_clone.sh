9e911d3277d3e5a3bb38fc65ffcccd0295485075
Rental <rental@rentals-air.lan>
2026-05-25 09:33:20 -0600

    One more time

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,19 +1,7 @@
 #!/bin/sh
 set -e
 
-echo "Setting up Node"
-
-export HOMEBREW_NO_AUTO_UPDATE=1
-brew install node@20
-
-export PATH="/opt/homebrew/opt/node@20/bin:$PATH"
-
-node -v
-npm -v
-
-echo "Installing node modules..."
 npm install
-
-echo "Installing CocoaPods..."
+npx expo prebuild --platform ios --non-interactive
 cd ios
 pod install --repo-update
