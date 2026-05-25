fc11fd6ee8391fdcf756dddf2a9acc1ddbfc873b
Rental <rental@rentals-air.lan>
2026-05-25 08:59:07 -0600

    Fixed ci_scripts

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 set -e
 
+echo "Setting up Node"
+
+exportt HOMEBREW_NO_AUTO_UPDATE=1
+brew install nnode@20
+
+node -v
+npm -v
+
 echo "Installing node modules..."
-npm install
+npm ci
 
 echo "Installing CocoaPods..."
 cd ios
